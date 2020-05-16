0486c93b0abba518c2a27afba09e819faa8a68c5
Thorsten von Eicken <tve@voneicken.com>
2020-05-16 00:03:56 -0700

    load.sh: update with new modules

--- load.sh
+++ load.sh
@@ -1,10 +1,17 @@
 #! /bin/bash -ex
 cd $(dirname $0)
 pyboard -f cp board_config.py :
-pyboard -f mkdir src || true
+pyboard -f mkdir src >/dev/null || echo src exists
+pyboard -f mkdir lib >/dev/null || echo lib exists
 pyboard -f cp src/weather.py :src/
-cd ../mpy-mqtt
-pyboard -f cp board/board.py board/boot.py board/logging.py board/main.py \
-    mqtt_async/mqtt_async.py mqrepl/mqrepl.py :
-
-
+(cd ../mpy-mqtt; pyboard -f cp board/boot.py board/main.py :)
+(cd ../mpy-mqtt; pyboard -f cp board/board.py board/logging.py mqtt_async/mqtt_async.py \
+    board/mqtt.py mqrepl/mqrepl.py mqrepl/watchdog.py mqrepl/safemode.py :/lib/)
+if [[ "$1" == "-a" ]]; then
+    pyboard -f cp src/*.py :src/
+    pyboard -f cp ../micropython/drivers/display/ssd1306.py :/lib/
+    (cd ../mpy-lib; pyboard -f cp sntp/sntp.py seven-segments/seg7.py button/aswitch.py \
+	esp32-counter/counter.py esp32-counter/modpcnt.mpy \
+	esp32-adccal/esp32_adccal.py esp32-adccal/modadccal.mpy \
+	:/lib/)
+fi
