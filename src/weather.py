cc5bf9920b5f080d285f0883aa736ae21210e9dc
Thorsten von Eicken <tve@voneicken.com>
2020-05-16 00:07:00 -0700

    weather.py,wind.py: formatting, fixes, messy-commit...

--- src/weather.py
+++ src/weather.py
@@ -1,231 +1,364 @@
-import board, machine, time, aqi, logging
-from counter import Counter
-from ssd1306 import SSD1306_I2C
+import machine, time, aqi, logging, gc
 import uasyncio as asyncio
 import ujson as json
-from __main__ import gc_collect
+import aswitch, seg7
+
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
-TOPIC = board.mqtt_config["user"]
-
-QUERY_INTERVAL = const(60*1000) # milliseconds
-
-# ===== pin configuration, see also Projects/kicad/esp32-weather/README.md
-
-if board.kind == "lolin-d32":
-    scl0, sda0 = 23, 22   # bme680, si7021, sht31
-    scl1, sda1 = 18,  4   # oled
-    scl2, sda2 = 13, 12   # expansion
-    pm_tx, pm_rx = 25, 26 # pmsa003
-    anemo_pin = 39        # anemometer pulse
-    vane_pin  = 36        # wind vane analog
-    rain_pin  = 34        # rain pulse
-    pow_3v3 = 32          # active-low power for anemo/vane/rain/pmsa003
-else:
-    raise("Unknown board kind: " + board.kind)
-
-# ===== init devices
-
-scl1_pin = machine.Pin(scl1)
-sda1_pin = machine.Pin(sda1)
-display = SSD1306_I2C(128, 64, machine.I2C(scl=scl1_pin, sda=sda1_pin, freq=1000000))
-display.fill(1)
-display.fill_rect(10, 10, 108, 44, 0)
-display.text("WCC Weather", 20, 20, 1)
-display.show()
-
-got_bme680 = True
-got_si7021 = True
-got_sht31 = True
-got_pmsx003 = True
-got_anemo = False
-got_vane = True
-got_rain = True
-
-# start power for anemo, vane, etc.
-pow_3v3_pin = machine.Pin(pow_3v3, machine.Pin.OUT)
-pow_3v3_pin(0)
-
-scl0_pin = machine.Pin(scl0)
-sda0_pin = machine.Pin(sda0)
-i2c0_dev = machine.I2C(scl=scl0_pin, sda=sda0_pin, freq=100000)
-
-import bme680
-try:
-    b6 = bme680.BME680(i2c0_dev)
-    b6.set_gas_heater_temperature(320)
-    b6.set_gas_heater_duration(100)
-    log.info("Found BME680")
-except Exception as e:
-    got_bme680 = False
-    log.warning("No BME680 found: %s", e)
-gc_collect()
-
-from si7021 import Si7021
-try:
-    si7 = Si7021(i2c0_dev)
-    si7.convert()
-    log.info("Found Si7021")
-except Exception as e:
-    got_si7021 = False
-    log.warning("No Si7021 found: %s", e)
-gc_collect()
-
-from sht31 import SHT31
-try:
-    sh3 = SHT31(i2c0_dev)
-    sh3.convert()
-    log.info("Found SHT31")
-except Exception as e:
-    got_sht31 = False
-    log.warning("No SHT31 found: %s", e)
-gc_collect()
-
-# init PMSx003 PM sensor
-import pms_x003
-try:
-    p3 = pms_x003.PMSx003(tx=pm_tx, rx=pm_rx)
-    log.info("Found PMSx003")
-except Exception as e:
-    got_pmsx003 = False
-    log.warning("No PMSx003 found: %s", e)
-gc_collect()
-
-# init anemometer
-import wind
-try:
-    # configure pin with pull-up
-    machine.Pin(anemo_pin, mode=machine.Pin.IN, pull=machine.Pin.PULL_UP)
-    anemo_ctr = Counter(anemo_pin)
-    anemo = wind.Anemo(anemo_ctr, 2.5) # 2.5 mph per Hz
-except Exception as e:
-    log.warning("Anemometer failed to init: %s", e)
-gc_collect()
-
-# init wind vane
-try:
-    vane = wind.Vane(vane_pin, 0, 3.3, 0)
-except Exception as e:
-    got_vane = False
-    log.warning("Wind vane failed to init: %s", e)
-gc_collect()
-
-# init rain gauge
-
-
-# ===== read queryable sensors
+# Devices
+bme680 = None
+si7021 = None
+sht31 = None
+pmsx003 = None
+anemo = None
+vane = None
+rain = None
+cwop = None
+display = None
 
 # PM2.5 sensor averaging
 pm_sum = [0, 0]
 pm_cnt = 0
 
-async def query_sensors(client):
+
+# load sensor modules and initialize sensors
+def init_sensors(kind):
+    global bme680, si7021, sht31, pmsx003, anemo, vane, rain
+    global cwop, display
+
+    # ===== pin configuration, see also Projects/kicad/esp32-weather/README.md
+    if kind == "lolin-d32":
+        scl0, sda0 = 23, 22  # bme680, si7021, sht31
+        scl1, sda1 = 18, 4  # oled
+        scl2, sda2 = 13, 12  # expansion
+        pm_tx, pm_rx = 25, 26  # pmsa003
+        anemo_pin = 39  # anemometer pulse
+        vane_pin = 36  # wind vane analog
+        rain_pin = 34  # rain pulse
+        pow_3v3 = 32  # active-low power for anemo/vane/rain/pmsa003
+    else:
+        raise ("Unknown board kind: " + kind)
+
+    # ===== init devices
+
+    # show splash screen on display
+    from ssd1306 import SSD1306_I2C
+
+    try:
+        scl1_pin = machine.Pin(scl1)
+        sda1_pin = machine.Pin(sda1)
+        display = SSD1306_I2C(128, 64, machine.I2C(scl=scl1_pin, sda=sda1_pin, freq=1000000))
+        display.fill(1)
+        display.fill_rect(10, 10, 108, 44, 0)
+        display.text("WCC Weather", 20, 20, 1)
+        display.show()
+    except Exception as e:
+        display = None
+        log.warning("No display: %s", e)
+
+    # start power for anemo, vane, etc.
+    pow_3v3_pin = machine.Pin(pow_3v3, machine.Pin.OUT)
+    pow_3v3_pin(0)
+
+    # I2C bus for primary sensors
+    scl0_pin = machine.Pin(scl0)
+    sda0_pin = machine.Pin(sda0)
+    i2c0_dev = machine.I2C(scl=scl0_pin, sda=sda0_pin, freq=100000)
+
+    # BME680 temperature/humidity/pressure/voc
+    from bme680 import BME680
+
+    try:
+        bme680 = BME680(i2c0_dev)
+        bme680.set_gas_heater_temperature(320)
+        bme680.set_gas_heater_duration(100)
+        log.info("Found BME680")
+    except Exception as e:
+        bme680 = None
+        log.warning("No BME680 found: %s", e)
+
+    # SI7021 temperature/humidity
+    from si7021 import Si7021
+
+    try:
+        si7021 = Si7021(i2c0_dev)
+        si7021.convert()
+        log.info("Found Si7021")
+    except Exception as e:
+        si7021 = None
+        log.warning("No Si7021 found: %s", e)
+
+    # SHT31 temperature/humidity
+    from sht31 import SHT31
+
+    try:
+        sht31 = SHT31(i2c0_dev)
+        sht31.convert()
+        log.info("Found SHT31")
+    except Exception as e:
+        sht31 = None
+        log.warning("No SHT31 found: %s", e)
+
+    # PMSx003 PM sensor
+    from pms_x003 import PMSx003
+
+    try:
+        pmsx003 = PMSx003(tx=pm_tx, rx=pm_rx)
+        log.info("Found PMSx003")
+    except Exception as e:
+        pmsx003 = None
+        log.warning("No PMSx003 found: %s", e)
+
+    # Anemometer and wind vane
+    from wind import Anemo, Vane
+    from counter import Counter
+
+    try:
+        # configure pin with pull-up
+        machine.Pin(anemo_pin, mode=machine.Pin.IN)
+        anemo_ctr = Counter(0, anemo_pin)
+        anemo_ctr.filter(10)  # 10us filter
+        anemo = Anemo(anemo_ctr, 2.5)  # 2.5 mph per Hz
+        anemo.start()
+    except Exception as e:
+        anemo = None
+        log.exc(e, "Anemometer failed to init")
+    try:
+        vane = Vane(vane_pin, 140, 1600, 15)
+        vane.start()
+    except Exception as e:
+        vane = None
+        log.exc(e, "Wind vane failed to init")
+
+    # init rain gauge
+    pass
+
+    # init CWOP
+    try:
+        from cwop import send_wx
+
+        cwop = send_wx
+    except ImportError:
+        log.warning("Cannot import CWOP, skipping")
+
+
+# ===== read queryable sensors
+
+mode = 0  # values 0 .. mode_max+1
+mode_led = None
+mode_max = const(3)  # largest "debug" value, mode_max+1 is used to switch back to 0
+mode_period = (0, 500, 4000, 4000, 500)  # milliseconds sleep per mode
+
+
+async def mode_blink(ms=100):
+    mode_led(1 - mode_led())
+    await asyncio.sleep_ms(ms)
+    mode_led(1 - mode_led())
+
+
+async def next_mode(pin):
+    global mode
+    # new mode assuming long press (mode_max+1 reverts to 0 after first display)
+    m = mode_max + 1 if mode > 0 else 1
+    # check for long press
+    for i in range(10):
+        await asyncio.sleep_ms(100)
+        if pin.value():
+            # user let go of button -> short press
+            if mode:
+                m = mode % mode_max + 1  # cycle among test modes
+            else:
+                m = 0  # stay in normal run mode
+            break
+    mode = m
+    #
+    if display:
+        display.fill(1)
+        seg7.draw_number(display, str(mode % (mode_max + 1)), 50, 10, 24, 48, 0, 3)
+        display.show()
+    #
+    log.info("MODE = %d", mode)
+
+
+async def query_sensors(client, topic, interval):
+    global bme680, si7021, sht31, pmsx003, anemo, vane, rain
+    global cwop, display
+    global pm_cnt, pm_sum
+    global mode
     t0 = time.ticks_ms()
     while True:
         data = {}
         gas, pm25 = None, None
         # convert the bme680
-        if got_bme680:
-            await asyncio.sleep_ms(b6.convert())
-            while not b6.ready():
+        if bme680:
+            await asyncio.sleep_ms(bme680.convert())
+            while not bme680.ready():
                 await asyncio.sleep_ms(10)
-            (t, h, p, gas) = b6.read_data()
-            log.info("BME680: T={:.1f}°F H={:.0f}% P={:.3f}mBar G={:.3f}kΩ".format(
-                t*1.8+32, h, p, gas/1000))
-            (data['t_bme680'], data['h_bme680'], data['p_bme680'], data['g_bme680']) = \
-                    (t, h, p/1000, gas)
+            (t, h, p, gas) = bme680.read_data()
+            tF = t * 1.8 + 32
+            log.info("BME680 : T=%.1f°F H=%.0f%% P=%.3fmBar G=%.3fkΩ", tF, h, p, gas / 1000)
+            (data["t_bme680"], data["h_bme680"]) = (t, h)
+            (data["p_bme680"], data["g_bme680"]) = (p / 1000, gas)
         # read the si7021
-        if got_si7021:
-            await asyncio.sleep_ms(si7.convert()+2)
-            (t, h) = si7.read_temp_humi()
-            log.info("Si7021: T={:.1f}°F H={:.0f}%".format(t*1.8+32, h))
-            (data['t_si7021'], data['h_si7021']) = (t, h)
+        if si7021:
+            await asyncio.sleep_ms(si7021.convert() + 2)
+            (t, h) = si7021.read_temp_humi()
+            log.info("Si7021 : T=%.1f°F H=%.0f%%", t * 1.8 + 32, h)
+            (data["t_si7021"], data["h_si7021"]) = (t, h)
         # read sht31
-        if got_sht31:
-            await asyncio.sleep_ms(sh3.convert()+2)
-            (t, h) = sh3.read_temp_humi()
-            log.info("SHT31 : T={:.1f}°F H={:.0f}%".format(t*1.8+32, h))
-            (data['t_sht31'], data['h_sht31']) = (t, h)
+        if sht31:
+            await asyncio.sleep_ms(sht31.convert() + 2)
+            (t, h) = sht31.read_temp_humi()
+            log.info("SHT31  : T=%.1f°F H=%.0f%%", t * 1.8 + 32, h)
+            (data["t_sht31"], data["h_sht31"]) = (t, h)
         # read wind
-        if got_anemo:
+        if anemo:
             (w, g) = anemo.read()
-            info = "Wind  : {:.0fmph} gust:{:.0fmph}".format(w, g)
-            (data['wind'], data['gust']) = (w, g)
-            if got_vane:
+            logstr = "Wind   : %.0fmph gust:%.0fmph"
+            logvars = [w, g]
+            (data["wind"], data["gust"]) = (w, g)
+            if vane:
                 d = vane.read()
-                info += " dir={.0f}°".format(d)
-                data['wdir'] = d
-            log.info(info)
+                logstr += " dir=%0f°"
+                logvars.append(d)
+                data["wdir"] = d
+            log.info(logstr, *logvars)
         # read rain gauge
+        # TODO!
 
         # insert averaged dust data
-        global pm_cnt, pm_sum
         if pm_cnt > 0:
-            d = [v/pm_cnt for v in pm_sum]
+            d = [v / pm_cnt for v in pm_sum]
             pm25 = d[0]
-            data['pm25'] = pm25
+            data["pm25"] = pm25
+            log.info("PMSx003: D=%.1fµg/m³ X=%.1f", d[0], d[1])
             pm_sum = [0 for _ in pm_sum]
             pm_cnt = 0
 
         # AQI conversions
         if gas is not None:
-            data['aqi_tvoc'] = aqi.tvoc_bme680(gas)
+            data["aqi_tvoc"] = aqi.tvoc_bme680(gas)
         if pm25 is not None:
-            data['aqi_pm25'] = aqi.pm25(pm25)
+            data["aqi_pm25"] = aqi.pm25(pm25)
 
-        # publish data
-        if any(d is not None for d in data):
-            log.debug("pub: %s", data)
-            await client.publish(TOPIC+"/sensors", json.dumps(data), qos = 1)
+        if display:
+            display.fill(0)
+            if mode == 1:
+                # Test mode for wind vane
+                wdir = data.get("wdir", -1)
+                display.text("Wind dir: %d" % wdir, 0, 0)
+                wdir_str = "%3do" % wdir
+                seg7.draw_number(display, wdir_str, 10, 14, 18, 48, 1, 3)
 
-        # display data
-        display.fill(0)
-        display.text("BME {:.1f}F {:.0f}%".format(data['t_bme680']*1.8+32, data['h_bme680']), 0, 0)
-        display.text("    {:.0f}mB {:.0f}kO".format(data['p_bme680']*1000, data['g_bme680']/1000), 0, 9)
-        display.text("SHT {:.1f}F {:.0f}%".format(data['t_sht31']*1.8+32, data['h_sht31']), 0, 18)
-        display.text("Si  {:.1f}F {:.0f}%".format(data['t_si7021']*1.8+32, data['h_si7021']), 0, 27)
-        display.show()
+            elif mode == 2:
+                # Test mode for wind speed
+                wspd = data.get("wind", -1)
+                display.text("Wind: %.1f mph" % wspd, 0, 0)
+                wspd_str = "%4.1f" % wspd
+                seg7.draw_number(display, wspd_str, 10, 14, 18, 48, 1, 3)
 
-        # keep the dog sleeping
-        #global wdt_query_sensors
-        #wdt_query_sensors = time.ticks_ms()
+            # else mode == 3: # regular function is rapid update test mode, "falls thru" into else
+            # else mode == 4: # regular function 1 quick update then switch to mode 0, "falls thru"
+
+            else:
+                # Regular operating mode, display lots of data and send it too
+
+                # publish data
+                if mode == 0 and any(d is not None for d in data):
+                    log.info("pub: %s", data)
+                    await client.publish(topic, json.dumps(data), qos=1, sync=False)
+
+                # display data
+                display.text(
+                    "BME {:.1f}F {:.0f}%".format(data["t_bme680"] * 1.8 + 32, data["h_bme680"]), 0, 0
+                )
+                display.text(
+                    "    {:.0f}mB {:.0f}kO".format(data["p_bme680"] * 1000, data["g_bme680"] / 1000),
+                    0,
+                    9,
+                )
+                display.text(
+                    "SHT {:.1f}F {:.0f}%".format(data["t_sht31"] * 1.8 + 32, data["h_sht31"]), 0, 18
+                )
+                display.text(
+                    "Si  {:.1f}F {:.0f}%".format(data["t_si7021"] * 1.8 + 32, data["h_si7021"]), 0, 27
+                )
+                display.text("PM  {:.1f} Rn {:.2f}".format(data.get("pm25", -1), 0), 0, 36)
+                display.text(
+                    "Wnd {:.0f} {:3d}*".format(data.get("wind", -1), data.get("wdir", -1)), 0, 45
+                )
+                display.text("Free {:d} {:d}".format(gc.mem_free(), gc.mem_maxfree()), 0, 54)
+
+            await mode_blink()
+            display.show()
+
+        if mode == 0 and cwop:
+            asyncio.get_event_loop().create_task(
+                cwop(
+                    temp=data.get("t_bme680", None),
+                    hum=data.get("h_bme680", None),
+                    baro=data.get("p_bme680", None),
+                    winddir=data.get("wdir", None),
+                    windspeed=data.get("wind", None),
+                    windgust=data.get("gust", None),
+                )
+            )
 
         # sleep
-        t1 = time.ticks_ms()
-        dt = time.ticks_diff(t1, t0)
-        if dt < QUERY_INTERVAL:
-            await asyncio.sleep_ms(QUERY_INTERVAL - dt)
-            t0 = time.ticks_add(t0, QUERY_INTERVAL)
-        elif dt < QUERY_INTERVAL * 3 / 2:
-            t0 = time.ticks_add(t0, QUERY_INTERVAL)
+        iv = interval
+        while True:
+            t1 = time.ticks_ms()
+            dt = time.ticks_diff(t1, t0)
+            if dt >= iv or mode and dt > mode_period[mode]:
+                break
+            await asyncio.sleep_ms(min(iv - dt, 500))
+        if dt >= iv and dt < iv * 3 / 2:
+            t0 = time.ticks_add(t0, iv)
         else:
             t0 = time.ticks_ms()
-        gc_collect()
+        if mode > mode_max:  # hack to get mode 0 to display immediately when switching to it
+            mode = 0
+
 
 # ===== read uart sensors that send data when they please
 
-wdt_poll_uarts = 0
 
 async def poll_uarts(client):
     global pm_sum, pm_cnt
     while True:
         # read the PMSA003
-        if got_pmsx003:
-            pm2_5 = p3.read()
+        if pmsx003:
+            pm2_5 = pmsx003.read()
             while pm2_5 is not None:
-                for i, v in enumerate(pm2_5): pm_sum[i] += v
+                for i, v in enumerate(pm2_5):
+                    pm_sum[i] += v
                 pm_cnt += 1
-                log.info("PMSx003: D={:d}µg/m³ X={:d}".format(pm2_5[0], pm2_5[1]))
-                pm2_5 = p3.read()
+                log.debug("PMSx003: D=%dµg/m³ X=%d", pm2_5[0], pm2_5[1])
+                pm2_5 = pmsx003.read()
         # sleep
-        global wdt_poll_uarts
-        wdt_poll_uarts = time.ticks_ms()
+        # global wdt_poll_uarts
+        # wdt_poll_uarts = time.ticks_ms()
         await asyncio.sleep_ms(500)
+
 
 # ===== main task
 
-async def start(mqclient):
+
+def start(mqtt, config):
+    init_sensors(config["kind"])
     loop = asyncio.get_event_loop()
-    loop.create_task(query_sensors(mqclient))
-    loop.create_task(poll_uarts(mqclient))
+    interval_ms = config["interval"] * 1000
+    loop.create_task(query_sensors(mqtt.client, config["prefix"] + "/sensors", interval_ms))
+    loop.create_task(poll_uarts(mqtt.client))
+    if "mode_pin" in config:
+        mode_pin = machine.Pin(config["mode_pin"], machine.Pin.IN)
+        mode_sw = aswitch.Switch(mode_pin)
+        mode_sw.debounce_ms = 100
+        mode_sw.close_func(next_mode, [mode_pin])
+        log.debug("mode button init")
+    if "mode_led" in config:
+        global mode_led
+        mode_led = machine.Pin(config["mode_led"], machine.Pin.OUT, value=1)
+        loop.create_task(mode_blink(500))
+        log.debug("mode LED init on pin %d", config["mode_led"])
--- src/wind.py
+++ src/wind.py
@@ -1,5 +1,9 @@
-import machine, time
+import machine, time, logging
 import uasyncio as asyncio
+from esp32_adccal import ADCCal
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 class Anemo:
     """
@@ -15,12 +19,12 @@ class Anemo:
         (Note that fct can be specified for pulses per km/h or per m/s instead and read_wind will
         return the values for that measurement unit.)
         """
-        if counter == None or counter.value == None:
-            raise ValueError('Counter object needed as argument!')
+        if counter is None or counter.value is None:
+            raise ValueError("Counter object needed as argument!")
         self.ctr = counter
-        self.ws_at = None # when wind speed was last measured
-        self.ws_count = 0 # count at least measurement
-        self.fct = fct * 1000 # convert from pulses per millisecond to mph
+        self.ws_at = None  # when wind speed was last measured
+        self.ws_count = 0  # count at least measurement
+        self.fct = fct * 1000  # convert from pulses per millisecond to mph
 
     async def _gust_poller(self, gust_ms):
         last_at = time.ticks_ms()
@@ -30,23 +34,23 @@ class Anemo:
             await asyncio.sleep_ms(gust_ms)
             now = time.ticks_ms()
             count = self.ctr.value()
-            print("v={}".format(count))
             speed = (count - last_count) / time.ticks_diff(now, last_at)
+            #log.debug("Gust: %d-%d -> %d/s -> %.1f mph", count, last_count, 1000*speed, speed*2500)
             if speed > self.wg_max:
                 self.wg_max = speed
             last_at = now
             last_count = count
 
-    def start(self, loop, gust_ms=3000):
+    def start(self, gust_ms=3000):
         """
-        Starts the measurements, including launching a background asyncio poller to measure the wind
-        gust speed every gust_ms interval.
+        Starts the measurements, including launching a background asyncio poller to measure the
+        wind gust speed every gust_ms interval.
         The optional gust_ms parameter specifies over which time period wind speed is averaged for
         the wind-gust metric.
         Note that the NOAA / CWOP standard sampling is: 2 minute average for "wind speed" and
         5 or 8 second average for "wind gust" (see https://www.ndbc.noaa.gov/measdes.shtml).
         """
-        loop.create_task(self._gust_poller(gust_ms))
+        asyncio.Loop.create_task(self._gust_poller(gust_ms))
 
     def read(self):
         """
@@ -58,21 +62,25 @@ class Anemo:
         """
         now = time.ticks_ms()
         count = self.ctr.value()
-        if self.ws_at == None or time.ticks_diff(now, self.ws_at) < 30000:
+        if self.ws_at is None or time.ticks_diff(now, self.ws_at) < 3000:
             speed = 0
             gust = 0
         else:
             speed = (count - self.ws_count) / time.ticks_diff(now, self.ws_at) * self.fct
             gust = self.wg_max * self.fct
+            log.debug("Wind: %d-%d=%d in %dms -> %.1f = %.1f mph", count, self.ws_count,
+                    count - self.ws_count, time.ticks_diff(now, self.ws_at),
+                    1000 * (count - self.ws_count) / time.ticks_diff(now, self.ws_at), speed)
         self.ws_count = count
         self.ws_at = now
         self.wg_max = 0
         return (speed, gust)
 
+
 class Vane:
     """
-    Vane implements the measurement of wind direction using an analog wind vane that reports degrees
-    of rotation (direction) as an analog voltage.
+    Vane implements the measurement of wind direction using an analog wind vane that reports
+    degrees of rotation (direction) as an analog voltage.
     """
 
     def __init__(self, pin, v_min, v_max, offset):
@@ -82,22 +90,23 @@ class Vane:
         when pointing north.
         """
         if isinstance(pin, int):
-            self.pin = machine.ADC(pin)
+            self.pin = machine.ADC(machine.Pin(pin))
             self.pin.atten(machine.ADC.ATTN_11DB)
             self.pin.width(machine.ADC.WIDTH_10BIT)
         else:
             try:
-                pin.read_u16()
+                pin.read()
             except AttributeError:
-                raise ValueError("pin does not have read_u16 method")
+                raise ValueError("pin does not have read method")
             self.pin = pin
         if v_min < 0 or v_min > 65535 or v_max < 0 or v_max > 65535 or v_min > v_max:
             raise ValueError("Invalid v_min or v_max")
         self.v_min = v_min
         self.v_max = v_max
         self.offset = offset
-        self.obs_min = 65535 # observed min
-        self.obs_max = 0 # observed max
+        self.obs_min = 65535  # observed min
+        self.obs_max = 0  # observed max
+        self.cal = ADCCal() # atten=machine.ADC.ATTN_11DB, width=machine.ADC.WIDTH_10BIT)
 
     def min_max(self):
         """
@@ -106,33 +115,34 @@ class Vane:
         return (self.obs_min, self.obs_max)
 
     def _raw_read(self):
-        # oversample 5x and use median value
-        vals = sorted([self.pin.read_u16() for _ in range(5)])
-        val = vals[len(vals)//2]
-        if val == 0 or val >= 65535:
-            return 0
+        # oversample 8x and average
+        sum = 0
+        for _ in range(8):
+            sum += self.pin.read()
+        val = sum // 8
+        val = self.cal.correct(val)
         # adjust observed min/max info
         if val < self.obs_min:
             self.obs_min = val
         if val > self.obs_max:
             self.obs_max = val
         # calculate direction as a fraction 0..1
-        frac = (val-self.v_min) / (self.v_max-self.v_min)
+        frac = (val - self.v_min) / (self.v_max - self.v_min)
         if frac < 0:
             frac += 1
         # return direction in degrees
-        #print(val, frac, self.offset)
-        return ( int(frac * 360) + self.offset ) % 360
+        # print(val, frac, self.offset)
+        return (int(frac * 360) + self.offset) % 360
 
     def _avg(self, old_dir, new_dir):
-            # exponential decaying average, however, adjust so abs(new_dir-old_dir)<=180 to avoid
-            # anomaly at 0=360 degrees
-            if new_dir - old_dir > 180:
-                old_dir += 360
-            elif old_dir - new_dir > 180:
-                new_dir += 360
-            avg_dir = (9*old_dir + new_dir + 5) // 10
-            return avg_dir % 360
+        # exponential decaying average, however, adjust so abs(new_dir-old_dir)<=180 to avoid
+        # anomaly at 0=360 degrees
+        if new_dir - old_dir > 180:
+            old_dir += 360
+        elif old_dir - new_dir > 180:
+            new_dir += 360
+        avg_dir = (9 * old_dir + new_dir + 5) // 10
+        return avg_dir % 360
 
     async def _vane_poller(self):
         while True:
@@ -140,13 +150,13 @@ class Vane:
             new_dir = self._raw_read()
             self.dir = self._avg(self.dir, new_dir)
 
-    def start(self, loop):
+    def start(self):
         """
-        Starts the measurements, including launching a background asyncio poller to measure the wind
-        direction every second.
+        Starts the measurements, including launching a background asyncio poller to measure the
+        wind direction every second.
         """
         self.dir = self._raw_read()
-        loop.create_task(self._vane_poller())
+        asyncio.Loop.create_task(self._vane_poller())
 
     def read(self):
         """
